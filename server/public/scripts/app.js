51ad1992cf76ca05e44f4c8c09ca3abedcaab613
Thomas Chester <thomcchester@gmail.com>
2016-03-06 16:38:44 -0600

    first

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,22 +1,69 @@
-var count=0;
+var count=-1;
 
 $(document).ready(function(){
     $.ajax({
       type: "GET",
       url: "/data",
       success: function(data){
-        console.log(data);
-        // appendDom(data);
         buttonMaker();
-        console.log(count);
-        $(".prev").on("click",buttonDown);
-        $(".next").on("click",buttonUp);
+        carniesMakingCarosels();
+        display(data);
+        eventListenersAndCarnieOpperators();
+        timer();
       }
     });
 });
 
+function resetClick(){
+  timercount=0;
+}
+function timer(){
+  var timercount=0
+  setInterval(function(){
+    $(".prev").click(function(){
+      timercount=0;
+    })
+    $(".next").click(function(){
+      timercount=0;
+    })
+    timercount++;
+    console.log(timercount)
+    if (timercount%10==0){
+      buttonUp();
+    }},500)
+}
+
+
+
+   function resetTimer() {
+       clearTimeout(t);
+       t = setTimeout(logout, 3000)
+   }
+
+function eventListenersAndCarnieOpperators(){
+  $(".prev").on("click",buttonDown);
+  $(".next").on("click",buttonUp);
+}
+function carniesMakingCarosels(){
+  $('.container').append('<div class="wheel"></div>');
+  $el=$('.container').children().last()
+  for (i=0;i<23;i++){
+    $el.append('<div class="pony"><p></p></div>');
+  }
+}
+
 function buttonMaker(){
-  $('body').append('<button class="prev">prev</button><button class="next">next</button>')
+  $('.container').append('<button class="prev">prev</button><button class="next">next</button>')
+}
+
+function display(data){
+  for (i=0;i<data.kappa.length;i++){
+    $('.container').append('<div class="person"><p>'+data.kappa[i].name +'</p></div>');
+    $el=$('.container').children().last();
+    $el.append('<p>'+data.kappa[i].location +'</p>');
+    $el.append('<p>'+data.kappa[i].spirit_animal +'</p>');
+    $el.append('<p>'+data.kappa[i].shoutout +'</p>');
+  }
 }
 
 function buttonUp(data){
@@ -26,68 +73,30 @@ function buttonUp(data){
   else{
     count++
   }
-  console.log(count)
-  console.log(data)
-
-
+  //make the events
+  $('.container').children('.person').eq(count-1).fadeOut("slow")
+  $('.container').children('.person').eq(count).delay(800).fadeIn("slow")
+  $('.container').children('.wheel').children('.pony').eq(count-1).removeClass("horse");
+  $('.container').children('.wheel').children('.pony').eq(count).addClass("horse");
+  timercount=0;
 }
 
 function buttonDown(){
+  //had to do it it a little different here, ran into soome timing issues so I did it this way.
+  $('.container').children('.person').eq(count).fadeOut("slow")
+  $('.container').children('.wheel').children('.pony').eq(count).removeClass("horse");
   if (count==0){
+    count=22
+  }else if(count==-1){
     count=22
   }
   else{
     count--
   }
- console.log(count)
+    $('.container').children('.person').eq(count).delay(800).fadeIn("slow")
+    $('.container').children('.wheel').children('.pony').eq(count).addClass("horse");
+
 }
 
-
-
-// // When pulling down this file, open it up in terminal and run 'npm install' in
-//  the terminal.
-// //
-// // Don't forget, to test an application with a node server, you will need to
-// enter 'npm start' (as defined in the package.json file).
-// //
-// // To shut down a server, hold down control and press 'C'.
-// //
-// // I created a new JSON data file, inside it, you will find an array of objects.
-//  Each object, is each one of you!
-// //
-
-// function appendDom(data){
-//   console.log(data.kappa.length)
-//   for(i=0;i<23;i++){
-//     console.log(data.kappa[i].name);
-//     $(".container").append('<div class="person"></p>');
-//     $el=$(".container").children().last();
-//     $el.append("<p class='name'> Name:  "+data.kappa[i].name+"</>");
-//     $el.append("<p class='location'>"+data.kappa[i].location+"</p>");
-//     $el.append("<p class='spirit_animal'>"+data.kappa[i].spirit_animal+"</p>");
-//     $el.append("<p class='shoutout'>"+data.kappa[i].shoutout+"</p>");
-//   };
-// };
-
-// // You first task is to make an AJAX call from the client side app.js, using the
-//  .ajax method, which will be set to GET, and access the '/data' url. When
-//  successful, it should bring the data back down. You will then need to combine
-// that with what you learned today about making a carousel.
-// //
-// // What I would like to see on the DOM, is one person represented. A series of
-//  22 (or the number of people in the cohort) index points with the first person's
-//   index highlighted or called out in style differently than the others. Also on
-//    the DOM should be a 'next' and 'prev' button. Clicking on the next button
-//    should navigate to the next person, clicking on the prev button should
-//    navigate to the previous person. The highlighted index point should update
-//     also as you click next and prev.
-// //
-// // When a person is displayed, show their name, their city, and their piece of
-//  shoutout feedback. Only one person should be show cased at any given time.
-// //
-// // You will need to combine everything you learned this week to accomplish this
-//  task, and each of the challenges you have completed this week play a part in
-//  this task.
-// //
-// // I posted an example from Zeta that I did here, so you can see the
-// functionality (Its ugly however), just know I minified the code (no cheating!):
+// one thing that I really dont like it if you click quick and fast more shows up
+// below. I could not quite figure out how to make that go away.
